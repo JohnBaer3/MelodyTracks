b6480caabffcc73ff334ad47db79a22db54240f2
Daniel Loi <dtloi@ucsc.edu>
2020-07-02 13:40:27 -0700

    added album cover and rearranged certain parts of UI

---
+++ MelodyTracks/Buttons/FowardButton.swift
@@ -0,0 +1,9 @@
+//
+//  FowardButton.swift
+//  MelodyTracks
+//
+//  Created by Daniel Loi on 7/1/20.
+//  Copyright © 2020 Daniel Loi. All rights reserved.
+//
+
+import Foundation
---
+++ MelodyTracks/Buttons/ShuffleButton.swift
@@ -0,0 +1,9 @@
+//
+//  ShuffleButton.swift
+//  MelodyTracks
+//
+//  Created by Daniel Loi on 7/1/20.
+//  Copyright © 2020 Daniel Loi. All rights reserved.
+//
+
+import Foundation
