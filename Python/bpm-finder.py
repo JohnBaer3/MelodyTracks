02153f1db86c005c4d6cb5af9d724888d4e322fd
michaelt0802 <michaelt0802@gmail.com>
2020-07-21 22:12:30 -0700

    added most recent ver of bpm-finder.py

--- Python/bpm-finder.py
+++ Python/bpm-finder.py
@@ -2,16 +2,20 @@
 # Michael Thompson
 # Input: music file 
 # Output: the music's bpm
-# Uses aubio, numpy, os libraries and ffmpeg
+# Uses aubio, numpy, os, sys, and ffmpeg (via pydub)
 # Adapted from aubio example library
 #
 # Song path must be defined before using
+#
+# Created: 6/30/20
+# Last Modified: 7/7/20
 ##############
 
 from aubio import source, tempo
 from numpy import median, diff
-import os
 from pydub import AudioSegment
+import os
+import sys
 
 # converts beats array to the song's BPM
 
@@ -27,15 +31,22 @@ def beats_to_bpm(beats, path):
         return 0
 
 if __name__ == '__main__':
+	# Check for audio file
+	if len(sys.argv) == 1:
+		print("Must provide music file")
+		sys.exit(1)
+
+	# Check for too many program arguments
+	if len(sys.argv) > 2:
+		print("Too many arguments")
+		sys.exit(1)
+
+
+    # User-supplied music file path
+	path = sys.argv[1]
 	samplerate, win_s, hop_s = 0, 256, 256
 
-	# User-supplied music file path
-	# uses relative path from working directory
-	dirname = os.path.abspath(os.path.dirname(__file__))
-	path = os.path.join(dirname, 'testfiles/thriller.mp3')
-
 	# Converts input audio file to .wav format for processing with aubio
-	# TODO do ffprobe to bypass conversion if input is already .wav
 	print('Converting song to wav...')
 	file = AudioSegment.from_file(path)
 	file.export ('file.wav', format='wav')
@@ -60,11 +71,9 @@ if __name__ == '__main__':
 	    if is_beat:
 	        this_beat = o.get_last_s()
 	        beats.append(this_beat)
-	        #if o.get_confidence() > .2 and len(beats) > 2.:
-	        #    break
 	    total_frames += read
 	    if read < hop_s:
 	        break
 
 	bpm = beats_to_bpm(beats, path)
-	print("{:6s} {:s}".format("{:2f}".format(bpm), path))
+	print("BPM:", "{:6s} {:s}".format("{:2f}".format(bpm),  path))
